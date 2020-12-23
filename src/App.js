f7372fda25d400a36d911093059695579d55b647
kimyb8870 <qhd0084@gmail.com>
2020-12-24 04:47:29 +0900

    Feature: app header with country selection form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { FormControl, Select, MenuItem, InputLabel } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
-      <h1>hello world</h1>
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl variant="outlined" className="app__dropdown">
+          <InputLabel>Country</InputLabel>
+          <Select label="Country" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
