f105842912c095fe079410ddcf657a0565e78f1c
kimyb8870 <qhd0084@gmail.com>
2020-12-24 21:13:33 +0900

    Feature: fetch corona cases of each countries at table

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import {
 } from "@material-ui/core";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
+import Table from "./Table";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -34,6 +36,7 @@ function App() {
             value: country.countryInfo.iso2,
           }));
 
+          setTableData(data);
           setCountries(countries);
         });
     };
@@ -99,7 +102,7 @@ function App() {
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by Country</h3>
-          {/* Table */}
+          <Table countries={tableData} />
           <h3>Worldwidd new cases</h3>
           {/* Graph */}
         </CardContent>
